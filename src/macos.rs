acc2000c20ffbf60999180b5560d90df624a74ea
unsecretised <no1umang@gmail.com>
2025-12-10 14:36:52 +0800

    Window settings

---
+++ src/macos.rs
@@ -0,0 +1,40 @@
+// ------ MacOS Configuration functions --------
+
+#[cfg(target_os = "macos")]
+use iced::wgpu::rwh::WindowHandle;
+
+#[cfg(target_os = "macos")]
+pub fn set_activation_policy_accessory() {
+    use objc2::MainThreadMarker;
+    use objc2_app_kit::{NSApp, NSApplicationActivationPolicy};
+
+    let mtm = MainThreadMarker::new().expect("must be on main thread");
+    let app = NSApp(mtm);
+    app.setActivationPolicy(NSApplicationActivationPolicy::Accessory);
+}
+
+#[cfg(target_os = "macos")]
+pub fn macos_window_config(handle: &WindowHandle) {
+    use iced::wgpu::rwh::RawWindowHandle;
+    use objc2::rc::Retained;
+    use objc2_app_kit::NSView;
+
+    match handle.as_raw() {
+        RawWindowHandle::AppKit(handle) => {
+            let ns_view = handle.ns_view.as_ptr();
+            let ns_view: Retained<NSView> = unsafe { Retained::retain(ns_view.cast()) }.unwrap();
+            let ns_window = ns_view
+                .window()
+                .expect("view was not installed in a window");
+            use objc2_app_kit::{NSMainMenuWindowLevel, NSWindowCollectionBehavior};
+
+            ns_window.setLevel(((NSMainMenuWindowLevel + 1) as u64).try_into().unwrap());
+            ns_window.setCollectionBehavior(NSWindowCollectionBehavior::CanJoinAllSpaces);
+        }
+        _ => {
+            panic!(
+                "Why are you running this as a non-appkit window? this is a macos only app as of now"
+            );
+        }
+    }
+}
